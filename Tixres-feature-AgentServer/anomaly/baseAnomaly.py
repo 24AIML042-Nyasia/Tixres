60a143a47183147ae0c156fcdc0b2b2b884904d6
Nyasia Patel <24aiml042@charusat.edu.in>
2026-02-10 07:14:27 +0530

    Remove obsolete AgentServer files

--- Tixres-feature-AgentServer/anomaly/baseAnomaly.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC , abstractmethod
-
-class BaseAnomaly(ABC):
-    detector_name = ''
-    @abstractmethod
-    def detect_anomaly(self):
-        pass
--- Tixres-feature-AgentServer/settings.py
+++
@@ -1,5 +0,0 @@
-DB_PATH = 'metrics_server.db'
-
-METRIC_LIST = []
-
-AGENT_LIST  = []
